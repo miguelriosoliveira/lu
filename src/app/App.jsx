6f44fb30e895f49c9bbd1bd061e29e1b7030ff95
Miguel Rios <miguel@adianta.com.br>
2020-02-11 22:39:20 -0300

    Using env vars

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -12,6 +12,73 @@ import './App.css';
 
 function App() {
   const [cep, setCep] = useState('');
+  const [snackbar, setSnackbar] = useState({ message: '', severity: 'info' });
+  const [address, setAddress] = useState({});
+  const [loading, setLoading] = useState(false);
+
+  function closeSnackbar() {
+    setSnackbar({ ...snackbar, message: '' });
+  }
+
+  function onChange(event) {
+    setCep(event.target.value);
+  }
+
+  function searchInMaps(address) {
+    if (address.erro) {
+      setSnackbar({ ...snackbar, message: 'CEP inválido', severity: 'error' });
+      return;
+    } else {
+      setAddress(address);
+    }
+  }
+
+  function removeMask(cep) {
+    return cep.replace(/\D/, '');
+  }
+
+  async function onSubmit(event) {
+    event.preventDefault();
+    setLoading(true);
+    let response = null;
+    let cepNumbers = removeMask(cep);
+    try {
+      response = await axios.get(`https://viacep.com.br/ws/${cepNumbers}/json/?callback=${searchInMaps.name}`);
+    } catch (error) {
+      setSnackbar({ ...snackbar, message: 'CEP inválido', severity: 'error' });
+      return;
+    } finally {
+      setLoading(false);
+    }
+
+    eval(response.data);
+  }
+
+  function renderLocation(address) {
+    // const { iframeKey } = this.state;
+    let baseUrl = `https://www.google.com/maps/embed/v1/search?key=${process.env.REACT_APP_GOOGLE_KEY}&q=`;
+    let location = `${address.logradouro}+${address.localidade}`;
+
+    if (address.logradouro && address.localidade) {
+      return (
+        <iframe
+          // key={iframeKey}
+          allowFullScreen
+          title="map-view"
+          frameBorder="0"
+          className="document-analysis__data-container__location"
+          src={`${baseUrl}${location}&language=pt-BR`}
+        />
+      );
+    } else {
+      return <Typography variant="caption">Localização não encontrada</Typography>;
+    }
+  }
+
+  function clear() {
+    setCep('');
+    setAddress({});
+  }
 
   return (
     <div className="app-component">
